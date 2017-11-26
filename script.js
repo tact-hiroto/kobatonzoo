2da4853105566c8d3d194dc9e79263a6f59b55ed
S-HIROTO-THOMAS\hiroto0629 <hirotothnew@outlook.jp>
2017-11-26 14:21:14 +0900

    kobatonzoo

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('#slider').bxSlider({
+    mode: 'horizontal',
+    speed: 2000,
+    pause: 8000,
+    auto: true,
+    pager: false,
+    controls: false,
+    touchEnabled: false
+  });
+});
